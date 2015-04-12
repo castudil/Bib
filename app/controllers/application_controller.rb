8c07f7858c5c455a931db5bab49413c218ddb0b9
gustavo000 <fenriz006@gmail.com>
2015-04-12 19:20:20 -0300

    permitted parameters to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,15 @@ class ApplicationController < ActionController::Base
   	allbibs_path
   end
 
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :avatar, :email, :password, :password_confirmation, :last_name, :biography , :graduation_year, :superUser  )
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:name, :avatar, :password, :password_confirmation, :current_password , :last_name, :biography , :graduation_year, :superUser  )
+    end
+  end
+
 
 end
---
+++ config/initializers/devise_permitted_parameters.rb
@@ -0,0 +1,19 @@
+# config/initializers/devise_permitted_parameters.rb
+
+module DevisePermittedParameters
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_parameters
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :avatar << :email << :password << :password_confirmation << :last_name << :biography << :graduation_year << :superUser
+    devise_parameter_sanitizer.for(:account_update) << :avatar << :email << :password << :password_confirmation << :last_name << :biography << :graduation_year << :superUser
+  end
+
+end
+
+DeviseController.send :include, DevisePermittedParameters
