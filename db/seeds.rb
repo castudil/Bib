eeeceae9687aad28851356f37f7134338e17e894
gustavo000 <fenriz006@gmail.com>
2015-04-13 10:15:27 -0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 
 
 	userAdmin = User.create([{ name: 'Cesar' , approved: true , last_name: 'Astudillo', biography: 'bio' , graduation_year: 1970 , 
-		email: 'email1@email.com' , password: 'password', superUser: 1 }])
+		email: 'email1@email.com' , career: 'teacher', password: 'password', superUser: 1 }])
 
 	user0 = User.create([{ name: 'Mario' , approved: true , last_name: 'Sanhueza', biography: '' , graduation_year: 1970 , 
-		email: 'email2@email.com' , password: 'password', superUser: 2 }])
+		email: 'email2@email.com' , career: 'Ing Industial', password: 'password', superUser: 2 }])
 
 	user1 = User.create([{ name: 'Jimmy' , approved: true , last_name: 'Gutierrez', biography: 'bio' , graduation_year: 1970 , 
-		email: 'email3@email.com' , password: 'password', superUser: 2 }])
+		email: 'email3@email.com' , career: 'Ing en Computacion',  password: 'password', superUser: 2 }])
 
 	user2 = User.create([{ name: 'Eduardo' , approved: true , last_name: 'Zapata', biography: 'bio' , graduation_year: 1970 , 
-		email: 'email4@email.com' , password: 'password', superUser: 2 }])
+		email: 'email4@email.com' ,  career: 'Ing en Computacion', password: 'password', superUser: 2 }])
