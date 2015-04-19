2e7027011753eb315e71964f0a212879379ae626
gustavo000 <fenriz006@gmail.com>
2015-04-19 01:53:37 -0300

    message of email worcking out ?

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -7,7 +7,7 @@ class InvitationMailer < MandrillMailer::MessageMailer
     # no need to set up template and template_content attributes, set up the html and text directly
     mandrill_mail subject: "hello",
                   to: "fenriz006@gmail.com",
-                  body: message
+                  text: message
                   # to: invitation.email,
                   # to: { email: invitation.email, name: 'Honored Guest' },
                   
