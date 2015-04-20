a20e8552290aa3457faf98d08c14004a2415f013
gustavo000 <fenriz006@gmail.com>
2015-04-19 22:50:08 -0300

    mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,19 @@
 class HomeController < ApplicationController
-	layout "main_home"
-	
+    layout "main_home"
+    
 
-	def index
-		@users = User.last(3) # last 3 records in ascending order
-		
-	end
+    def index
+        @users = User.last(3) # last 3 records in ascending order
+        
+    end
 
-	def contact
-	
-		InvitationMailer.contact_email(params[:message][:message]).deliver
+    def contact
+    
+        InvitationMailer.contact_email(params[:message][:message], params[:email][:email]).deliver
 
-		redirect_to root_path
-		
-	end 
+        redirect_to root_path
+        
+    end 
 
 
 end
--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,13 +1,14 @@
 class InvitationMailer < MandrillMailer::MessageMailer
   default from: 'support@example.com'
 
-  def contact_email(message)
+  def contact_email(message,email)
 
 
     # no need to set up template and template_content attributes, set up the html and text directly
     mandrill_mail subject: "hello",
                   to: "fenriz006@gmail.com",
-                  text: message
+                  text: "email : "+email +" message : "+message
+                  
                   # to: invitation.email,
                   # to: { email: invitation.email, name: 'Honored Guest' },
                   
