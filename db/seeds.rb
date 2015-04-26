b06d44db6095be98adaf871689b67d104403ed2c
gustavo000 <fenriz006@gmail.com>
2015-04-26 19:08:51 -0300

    seed biography added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,12 @@
 	userAdmin = User.create([{ name: 'Cesar' , approved: true , last_name: 'Astudillo', biography: 'bio' , graduation_year: 1970 , 
 		email: 'email1@email.com' , career: 'teacher', password: 'password', superUser: 1 }])
 
-	user0 = User.create([{ name: 'Mario' , approved: true , last_name: 'Sanhueza', biography: '' , graduation_year: 1970 , 
+	user0 = User.create([{ name: 'Mario' , approved: true , last_name: 'Sanhueza', biography: 'I was born in rancagua and then move to curico to study' ,
+	   graduation_year: 1970 , 
 		email: 'email2@email.com' , career: 'Ing en Computacion', password: 'password', superUser: 2 }])
 
-	user1 = User.create([{ name: 'Jimmy' , approved: true , last_name: 'Gutierrez', biography: 'bio' , graduation_year: 1970 , 
+	user1 = User.create([{ name: 'Jimmy' , approved: true , last_name: 'Gutierrez', biography: 'I was born in 1987 now i made a magister of industrial ingenier' , graduation_year: 1970 , 
 		email: 'email3@email.com' , career: 'Ing Industial',  password: 'password', superUser: 2 }])
 
-	user2 = User.create([{ name: 'Eduardo' , approved: true , last_name: 'Zapata', biography: 'bio' , graduation_year: 1970 , 
+	user2 = User.create([{ name: 'Eduardo' , approved: true , last_name: 'Zapata', biography: 'I was born in San Fernando and then move to curico to study' , graduation_year: 1970 , 
 		email: 'email4@email.com' ,  career: 'Ing en Computacion', password: 'password', superUser: 2 }])
