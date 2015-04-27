cb2d99c46f9b3c7cd294cc79853d333f4bc87813
gustavo000 <fenriz006@gmail.com>
2015-04-27 10:06:20 -0300

    recapcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,9 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LdL7wUTAAAAAIpQ9f7ar9O4neHTeBJ24bGoyq6l'
+  config.private_key = '6LdL7wUTAAAAAATEKMbJfdBvNyq8eAl2Arm_Jlqu'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  # config.api_version = 'v2'
+end
