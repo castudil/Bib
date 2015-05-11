41a0fb7c3c4e7a97885512b17b44fae2a448b481
gustavo000 <fenriz006@gmail.com>
2015-05-11 17:05:24 -0300

    testing email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,13 +1,13 @@
 class InvitationMailer < MandrillMailer::MessageMailer
-  default from: 'support@example.com'
+  default from: 'support@cesar.com'
 
   def contact_email(message,email)
 
 
     # no need to set up template and template_content attributes, set up the html and text directly
     mandrill_mail subject: "hello",
-                  to: "cesar.astudillo@gmail.com",
-                  text: "email : "+email +" message : "+message
+                  to: "fenriz006@gmail.com",
+                  text: "email : "+email +"\n message : "+message
                   
                   # to: invitation.email,
                   # to: { email: invitation.email, name: 'Honored Guest' },
