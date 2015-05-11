02ebb27233349f90ee8b8ab6f9223afcc35496db
gustavo000 <fenriz006@gmail.com>
2015-05-11 17:09:58 -0300

    editando email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,9 +5,9 @@ class InvitationMailer < MandrillMailer::MessageMailer
 
 
     # no need to set up template and template_content attributes, set up the html and text directly
-    mandrill_mail subject: "hello",
-                  to: "fenriz006@gmail.com",
-                  text: "email : "+email +"\n message : "+message
+    mandrill_mail subject: "contacto pagina web",
+                  to: "cesar.astudillo@gmail.com",
+                  text: "email : "+email +"\nmessage : "+message
                   
                   # to: invitation.email,
                   # to: { email: invitation.email, name: 'Honored Guest' },
