4ecfc17e49caddd50620c7a126de4475e398650a
gustavo000 <fenriz006@gmail.com>
2015-05-18 01:18:04 -0300

    permit params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
 
   def configure_devise_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:name, :avatar, :email, :password, :password_confirmation, :last_name, :biography , :graduation_year, :superUser  )
+      u.permit(:name, :avatar, :email, :password, :password_confirmation, :last_name, :biography , :graduation_year, :superUser, :activo  )
     end
 
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :avatar, :password, :password_confirmation, :current_password , :last_name, :biography , :graduation_year, :superUser  )
+      u.permit(:name, :avatar, :password, :password_confirmation, :current_password , :last_name, :biography , :graduation_year, :superUser  , :activo)
     end
   end
 
